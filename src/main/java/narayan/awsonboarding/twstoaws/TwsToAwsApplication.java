ceccfda35a401bcdbc89d1a785a959ffdcd95adc
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-08-11 14:38:11 +0530

    Initial working version with basic functions

--- src/main/java/narayan/awsonboarding/twstoaws/TwsToAwsApplication.java
+++ src/main/java/narayan/awsonboarding/twstoaws/TwsToAwsApplication.java
@@ -1,12 +1,48 @@
 package narayan.awsonboarding.twstoaws;
 
+import java.util.function.Function;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+import org.springframework.context.annotation.Bean;
 
+/**
+ * TWS to AWS OnBoarding POC
+ * 
+ * @author narayana
+ *
+ */
 @SpringBootApplication
-public class TwsToAwsApplication {
+public class TwsToAwsApplication extends SpringBootRequestHandler<String, String> {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TwsToAwsApplication.class, args);
+	}
+
+	/**
+	 * <pre>
+	 * Endpoing to invoke:
+	 * curl localhost:8080/uppercase -d '"Alpha_Beta_Gamma"' -H "Content-Type: application/json"
+	 * </pre>
+	 * 
+	 * @return
+	 */
+	@Bean
+	public Function<String, String> uppercase() {
+		return req -> req.toUpperCase();
+	}
+
+	/**
+	 * <pre>
+	 * Endpoing to invoke:
+	 * curl localhost:8080/lowercase -d '"Alpha_Beta_Gamma"' -H "Content-Type: application/json"
+	 * </pre>
+	 * 
+	 * @return
+	 */
+	@Bean
+	public Function<String, String> lowercase() {
+		return req -> req.toLowerCase();
 	}
 }
