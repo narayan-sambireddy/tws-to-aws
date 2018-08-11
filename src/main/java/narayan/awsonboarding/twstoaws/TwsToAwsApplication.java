86acf0f5b4c079717b6c47e0584f45607f3215b0
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-08-11 13:52:49 +0530

    Initial commit

---
+++ src/main/java/narayan/awsonboarding/twstoaws/TwsToAwsApplication.java
@@ -0,0 +1,12 @@
+package narayan.awsonboarding.twstoaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwsToAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwsToAwsApplication.class, args);
+	}
+}
---
+++ src/test/java/narayan/awsonboarding/twstoaws/TwsToAwsApplicationTests.java
@@ -0,0 +1,16 @@
+package narayan.awsonboarding.twstoaws;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwsToAwsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
